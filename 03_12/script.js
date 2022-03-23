7a406f7fc8839350dc74dc257667c258a344e194
najwanan <najwanan@hotmail.com>
2022-03-23 14:43:02 -0700

    completed practice 05_04 DOM modification

---
+++ 03_12/script.js
@@ -0,0 +1,13 @@
+import Backpack from './Backpack'
+
+const slingPurse = new Backpack(
+    "Sling Purse",
+    15,
+    "Brown",
+    10,
+    12,
+    12,
+    true
+);
+
+console.log(slingPurse)
--- 05_04/script.js
+++ 05_04/script.js
@@ -2,3 +2,18 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+
+let listItems = document.querySelector(".backpack__features");
+
+let pageHeader = document.querySelector(".page-header__heading");
+
+let backpackName = document.querySelectorAll(".backpack__name")
+
+//listItems.style.setProperty("list-style-type", "none")
+
+pageHeader.style.color = "#F2B705";
+
+backpackName.forEach(element => {
+      element.style.backgroundColor = "blue"    
+});
