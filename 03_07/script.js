437bc5e3c709c2013792a53740008576f978a206
najwanan <najwanan@hotmail.com>
2022-03-22 21:25:31 -0700

    completed Object Properties exercise

--- 03_07/script.js
+++ 03_07/script.js
@@ -7,3 +7,21 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+ 
+const purse = {
+    name: 'mini-backpack',
+    color: 'blue',
+    brand: 'Generic',
+    strapLength: {
+        left: 26, 
+        right: 26,
+    },
+    flapOpen: false,
+    togglePurse: function(flapStatus) {
+        this.flapOpen = flapStatus; 
+    },
+};
+ 
+var query = 'name';
+
+console.log(purse[query])
